460868ef665a89d63ff5f1b5fd67afc8fa666488
ErickDev <ericksuper80@hotmail.com>
2026-01-16 11:21:21 -0600

    Este Es Mi Octavo Commit

--- Module_Own.py
+++ Module_Own.py
@@ -1,16 +1,16 @@
-Diccionario_Poke = dict.fromkeys(['Poke1', 'Poke2', 'Poke3'])
+Diccionario_Poke = dict.fromkeys(["Poke1", "Poke2", "Poke3"])
 
-Set_Conjunto_Poke = set({'Pikachu'})
+Set_Conjunto_Poke = {'Pikachu'}
 Set_Conjunto_Poke.add('Graveler')
 Set_Conjunto_Poke.add('Vaporeon')
 
 for indice, elemento in enumerate(Set_Conjunto_Poke, start=1):
-    if (elemento == "Pikachu"):
-        Diccionario_Poke["Poke1"] = elemento
-    elif (elemento == "Graveler"):
-        Diccionario_Poke["Poke2"] = elemento
-    elif (elemento == "Vaporeon"):
-        Diccionario_Poke["Poke3"] = elemento
+    if (elemento == 'Pikachu'):
+        Diccionario_Poke['Poke1'] = elemento
+    elif (elemento == 'Graveler'):
+        Diccionario_Poke['Poke2'] = elemento
+    elif (elemento == 'Vaporeon'):
+        Diccionario_Poke['Poke3'] = elemento
     else:
         continue
 
@@ -58,15 +58,15 @@ def Primera(Segunda):
 
 @Primera
 def Par(Numero):
-    if (Numero % 2 == 0):
+    if (Numero % 2 ==0):
         return True
     else:
         return False
 
 def Primera(Segunda):
     def Tercera(*args, **kwargs):
-        Nombre = "Juanita La Cubanita"
-        Sexo = "FEMENINO"
+        Nombre = 'Juanita La Cubanita'
+        Sexo = 'FEMENINO'
         return Segunda(Nombre, Sexo)
 
     return Tercera
@@ -74,14 +74,14 @@ def Primera(Segunda):
 @Primera
 def Usuario(Nombre, Sexo):
     Genero = Sexo.lower()
-    if (Genero == "masculino"):
+    if (Genero == 'masculino'):
         print (f'{Nombre}, eres un hombre')
     else:
         print (f'{Nombre}, eres una mujer')
 
 def Primera(Segunda):
     def Tercera(*args):
-        return Segunda(27)
+        return Segunda(99)
 
     return Tercera
 
@@ -96,24 +96,24 @@ def Contrasena(Numero):
     Password = f'{chars[c1]}{chars[c2]}{chars[c3]}{int(abs(Numero * c2))}'
     return Password
 
-Lista_Numeros = list([])
-Lista_Numeros.extend([1, 2, 3])
-Lista_Numeros.append(5)
-Lista_Numeros.insert(2, 4)
+Lista_Numeros = []
+Lista_Numeros.append(1)
+Lista_Numeros.extend([2, 3, 4])
+Lista_Numeros.insert(4, 5)
 
 print (f'{Lista_Numeros}')
 
 Variable_Funcion_Anonima1 = lambda Num1, Num2 : Num1 * Num2
 Variable_Funcion_Anonima2 = lambda Num : Num * 2
-Variable_Funcion_Anonima3 = filter(lambda Num : Num % 2 == 0, Lista_Numeros)
+Variable_Funcion_Anonima3 = filter(lambda Num :  Num % 2 == 0, Lista_Numeros)
 
-Any_Pares = any(num % 2 == 0 for num in Lista_Numeros)
-Lista_Pares = [num for num in Lista_Numeros if num % 2 == 0]
+Any_Par = any(num % 2 == 0 for num in Lista_Numeros)
+Lista_Par = [num for num in Lista_Numeros if num % 2 == 0]
 
-print (f'{Any_Pares}')
-print (f'{Lista_Pares}')
+print (f'{Any_Par}')
+print (f'{Lista_Par}')
 
-Global = 30
+Var_Global = 30
 
 class Pokemon:
     def __init__(self, Nombre, Tipo, Ataque):
@@ -128,17 +128,11 @@ class Pokemon:
         print (f'Tipo: {self.Tipo}')
         print (f'Ataque: {self.Ataque}')
 
-Division_Flotante = 14/7
+Division_Flotante = 14//7
 
 Lista1 = ["Erick", "Josue", "Perez", "Gutierrez"]
-Lista2 = [36, Division_Flotante, Diccionario_Poke["Poke2"], True]
+Lista2 = [36, Division_Flotante, 'Koala', True]
 Lista3 = list([1, 2, 3, 4, 5])
-Lista4 = [4000, 15, 95, 200]
+Lista4 = [4000, 15, 97, 200]
 
 Tupla_Poke = ('Ash', 'Brooke', 'Misty')
-
-Flotante1 = int(input(f'Ingrese un numero: '))
-
-Flotante2 = input(f'Ingrese una operacion tipo 4*3: ')
-
-Flotante3 = input(f'Ingrese una cadena de texto: ')
