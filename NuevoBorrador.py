878468a2ccdb8db164e9bdde8637ab64eb38b1cd
ErickDev <ericksuper80@hotmail.com>
2026-01-20 12:52:01 -0600

    NuevoBorrador commit 4

--- NuevoBorrador.py
+++ NuevoBorrador.py
@@ -7,3 +7,8 @@ def Saludar2(Nombre = 'Juanita'):
     return Nombre
 
 print (f'Hola {Saludar2()}')
+
+def Saludar3(Nombre:str) -> str:
+    return Nombre
+
+print (f'Hola nuevamente {Saludar3(Saludar2())}')
