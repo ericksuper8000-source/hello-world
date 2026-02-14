fb8d87e4d7cf26228a7598271d114366bad70eb2
ErickDev <ericksuper80@gmail.com>
2026-02-14 13:25:46 -0600

    Feature 3 commit 1

--- Module_Own.py
+++ Module_Own.py
@@ -127,3 +127,12 @@ class Pokemon:
         print (f'Nombre: {self.Nombre}')
         print (f'Tipo: {self.Tipo}')
         print (f'Ataque: {self.Ataque}')
+
+Division_Flotante = 14/7
+
+Lista1 = ['Erick', 'Josue', 'Perez', 'Gutierrez']
+Lista2 = list([Division_Flotante, 36, 'Koala', False])
+Lista3 = [1, 2, 3, 4, 5]
+Lista4 = [4000, 97, 15, 200]
+
+Tupla_Poke = ('Ash', 'Brooke', 'Misty')
