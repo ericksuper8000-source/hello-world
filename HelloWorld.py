4f2c1936948e100ef978258a904e9fb24a51c623
Erick_Dev <ericksuper80@gmail.com>
2026-04-12 15:29:34 -0600

    feature10 commit 2

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,5 @@
 import fontTools.tfmLib
+import numpy as np
 
 try:
     import Module_Own as PEPE
@@ -315,3 +316,517 @@ print (f'-' * 20)
 Buscar12 = re.findall(r'(0[1-9]|[12][0-9]|3[01])', '32')
 
 print (f'{Buscar12}')
+
+Buscar13 = bool(re.match(r'(0[0-9]|[12][0-9]|3[01])', '32'))
+
+if (Buscar13 == True):
+    print (f'El numero se encuentra entre 00 y 31')
+else:
+    print (f'Error, no se encuentra en el rango definido')
+
+def Exception1(Elemento):
+    try:
+        Numerito = int(Elemento)
+        print (f'El elemento es un numero {Numerito}')
+    except ValueError:
+        print (f'Error, necesito que ingreses un numero')
+
+Exception1("Hola")
+
+def Exception2(Num1, Num2):
+    try:
+        Resultado = Num1 + Num2
+        print (f'El resultado de la operacion es {Resultado}')
+    except TypeError:
+        print (f'Error, ambos elementos deben ser numeros')
+
+Exception2(12, "Hola")
+
+def Exception3(Num1, Num2):
+    try:
+        Divi = Num1 / Num2
+        print (f'El resultado de la division es {round(Divi, 2)}')
+    except ZeroDivisionError:
+        print (f'Error, el divisor no puede ser un numero')
+
+Exception3(12, 0)
+
+Lista_Exception4 = list(['Erick', 'Josue', 'Karlita'])
+
+def Exception4(Indice):
+    try:
+        print (f'El elemento en la posicion {Indice} es {Lista_Exception4[Indice]}')
+    except IndexError:
+        print (f'Error, El indice esta fuera de rango')
+
+Exception4(3)
+
+Diccionario_Exception5 = {
+    'Nombre' : "Erick",
+    'Edad' : 37
+}
+
+def Exception5(Llave):
+    try:
+        print (f'El elemento en la llave {Llave} es {Diccionario_Exception5[Llave]}')
+    except KeyError:
+        print (f'Error, la llave seleccionada esta fuera de rango')
+
+Exception5('Votante')
+
+Ruta_Txt = 'C:\\Repo\\HolaMundo.txt'
+
+try:
+    with open('C:\\Repo\\HolaMundo.txt', 'w', encoding='UTF-8') as Docu:
+        Documento_SobreEscribir = Docu.write(f'Durazno')
+        Docu.close()
+except FileNotFoundError:
+    print (f'Error, el archivo no fue encontrado')
+
+with open ('C:\\Repo\\HolaMundo.txt', encoding='UTF-8') as Docu:
+    Documento_Linea = Docu.readline()
+    print (f'{Documento_Linea}')
+    Docu.close()
+
+with open ('C:\\Repo\\HolaMundo.txt', 'a', encoding='UTF-8') as Docu:
+    Documento_Agregar = Docu.writelines([f'\nManzana'])
+    Docu.close()
+
+with open ('C:\\Repo\\HolaMundo.txt', encoding='UTF-8') as Docu:
+    Documento_Lineas = Docu.readlines()
+    print (f'{Documento_Lineas}')
+    Docu.close()
+
+with open ('C:\\Repo\\HolaMundo.txt', 'a', encoding='UTF-8') as Docu:
+    Documento_Agregar = Docu.write(f'\nUvas')
+    Docu.close()
+
+with open ('C:\\Repo\\HolaMundo.txt', encoding='UTF-8') as Docu:
+    Documento_Leer = Docu.read()
+    print (f'{Documento_Leer}')
+    Docu.close()
+
+with open ('C:\\Repo\\HolaMundo.txt', 'a', encoding='UTF-8') as Docu:
+    Documento_Agregar = Docu.writelines([f'\nFresas Sabrosas', '\nFresas Sabrosas', '\nFresas Sabrosas'])
+    Docu.close()
+
+with open ('C:\\Repo\\HolaMundo.txt', encoding='UTF-8') as Docu:
+    Documento_Linea = Docu.readline()
+    print (f'{Documento_Linea}')
+    Docu.close()
+
+with open ('C:\\Repo\\HolaMundo.txt', 'a', encoding='UTF-8') as Docu:
+    Documento_Agregar = Docu.write(f'\n{PEPE.Diccionario_Poke["Poke1"]}')
+    Documento_Agregar = Docu.write(f'\n{PEPE.Diccionario_Poke["Poke2"]}')
+    Documento_Agregar = Docu.write(f'\n{PEPE.Diccionario_Poke["Poke3"]}\n')
+    Docu.close()
+
+with open ('C:\\Repo\\HolaMundo.txt', encoding='UTF-8') as Docu:
+    Documento_Lineas = Docu.readlines()
+    print (f'{Documento_Lineas}')
+    Docu.close()
+
+with open ('C:\\Repo\\HolaMundo.txt', 'a', encoding='UTF-8') as Docu:
+    Documento_Agregar = Docu.writelines([f' - '.join(PEPE.Set_Conjunto_Poke)])
+    Docu.close()
+
+with open ('C:\\Repo\\HolaMundo.txt', encoding='UTF-8') as Docu:
+    Documento_Leer = Docu.read()
+    print (f'{Documento_Leer}')
+    Docu.close()
+
+import pandas as pd
+
+Data_Frame1 = pd.DataFrame({
+    'Nombre' : ["Erick", "Josue", "Karlita"],
+    'Edad' : [37, 20, 6],
+    'Votante' : [True, not False, False]
+})
+
+Data_Frame2 = pd.DataFrame({
+    'Nombre' : ["Carmelo", "Susanita", "Roxana"],
+    'Edad' : [55, 14, 26],
+    'Votante' : [True, False, not False]
+})
+
+Data_Frame_Concatenate = pd.concat([Data_Frame1, Data_Frame2])
+
+print (f'{Data_Frame_Concatenate}')
+
+print (f'-' * 20)
+
+Data_Frame_Concatenate_Age = Data_Frame_Concatenate['Edad']
+
+print (f'{Data_Frame_Concatenate_Age}')
+
+print (f'-' * 20)
+
+print (f'{Data_Frame_Concatenate.info()}')
+
+print (f'-' * 20)
+
+print (f'El numero menor del dataframe es {Data_Frame_Concatenate_Age.min()} y el mayor de los numeros es {Data_Frame_Concatenate_Age.max()}')
+
+for indice, elemento in Data_Frame_Concatenate.iterrows():
+    Nombrecito = elemento['Nombre']
+
+    print (f'Mi nombre es {Nombrecito}')
+
+print (f'-' * 20)
+
+import pandas as pd
+
+Ruta_Csv1 = 'C:\\Repo\\Store.csv'
+
+Cargar_Csv1 = pd.read_csv(Ruta_Csv1)
+
+print (f'{Cargar_Csv1}')
+
+Group1 = Cargar_Csv1.groupby('product')['quantity'].sum()
+
+Group1_May = Group1.idxmax()
+Group1_Min = Group1.idxmin()
+Group1_May_Cant = Group1.max()
+Group1_Min_Cant = Group1.min()
+
+print (f'El producto menos vendido fue ({Group1_Min}) con un total de {Group1_Min_Cant} unidades y el producto mas vendido fue ({Group1_May}) con un total de {Group1_May_Cant} unidades')
+
+'''import pandas as pd
+import matplotlib.pyplot as plt
+
+x = list([1, 2, 3, 4])
+y = [45, 77, 20, 95]
+
+plt.figure(figsize=(4,3))
+plt.plot(x, y)
+
+plt.title("Ventas")
+plt.xlabel("Día")
+plt.ylabel("Cantidad")
+
+plt.show()
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.lineplot(x = 'Nombre', y = 'Edad', data=Data_Frame_Concatenate)
+
+plt.show()
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.barplot(x = 'Nombre', y = 'Edad', data=Data_Frame_Concatenate)
+
+plt.show()
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.scatterplot(x = 'Nombre', y = 'Edad', data=Data_Frame_Concatenate)
+
+plt.show()'''
+
+print (f'-' * 20)
+
+print (f'{Data_Frame_Concatenate.head(3)}')
+
+print (f'-' * 20)
+
+print (f'{Data_Frame_Concatenate.head(1)}')
+
+print (f'-' * 20)
+
+print (f'{Data_Frame_Concatenate.tail(1)}')
+
+print (f'-' * 20)
+
+Filas, Columnas = Data_Frame_Concatenate.shape
+
+print (f'Filas: {Filas}')
+print (f'Columnas: {Columnas}')
+
+Elemento1 = Data_Frame1.loc[0, 'Nombre']
+Elemento2 = Data_Frame1.loc[1, 'Edad']
+Elemento3 = Data_Frame1.loc[2, 'Votante']
+Elemento4 = Data_Frame1.loc[:, 'Nombre']
+Elemento5 = Data_Frame1.loc[2, :]
+
+print (f'{Elemento1}')
+print (f'{Elemento2}')
+print (f'{Elemento3}')
+print (f'{Elemento4}')
+print (f'{Elemento5}')
+
+print (f'-' * 20)
+
+Elemento6 = Data_Frame2.iloc[0, 0]
+Elemento7 = Data_Frame2.iloc[1, 1]
+Elemento8 = Data_Frame2.iloc[2, 2]
+Elemento9 = Data_Frame2.iloc[1, :]
+Elemento10 = Data_Frame2.iloc[:, 0]
+
+print (f'{Elemento6}')
+print (f'{Elemento7}')
+print (f'{Elemento8}')
+print (f'{Elemento9}')
+print (f'{Elemento10}')
+
+print (f'-' * 20)
+
+import pandas as pd
+import openpyxl
+
+Ruta_Excel = 'C:\\Repo\\Book.xlsx'
+
+Cargar_Excel = pd.read_excel(Ruta_Excel)
+
+print (f'{Cargar_Excel.head()}')
+
+print (f'-' * 20)
+
+Cargar_Excel1 = pd.read_excel(Ruta_Excel, sheet_name=1)
+Cargar_Excel2 = pd.read_excel(Ruta_Excel, sheet_name=0, header=0)
+Cargar_Excel3 = pd.read_excel(Ruta_Excel, sheet_name=0, header=0, names=['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez'])
+Cargar_Excel4 = pd.read_excel(Ruta_Excel, sheet_name=0, header=0, index_col='tiquete')
+Cargar_Excel5 = pd.read_excel(Ruta_Excel, sheet_name=0, header=0, usecols='E:K', index_col='tiquete')
+Cargar_Excel6 = pd.read_excel(Ruta_Excel, sheet_name=0, header=0, usecols='E:K', index_col='tiquete', nrows=1)
+
+print (f'{Cargar_Excel1.head()}')
+
+print (f'-' * 20)
+
+print (f'{Cargar_Excel2.head()}')
+
+print (f'-' * 20)
+
+print (f'{Cargar_Excel3.head()}')
+
+print (f'-' * 20)
+
+print (f'{Cargar_Excel4.head()}')
+
+print (f'-' * 20)
+
+print (f'{Cargar_Excel5.head()}')
+
+print (f'-' * 20)
+
+print (f'{Cargar_Excel6.head()}')
+
+print (f'-' * 20)
+
+Cargar_Excel3_Sorted = Cargar_Excel3.sort_values(by='cinco', ascending=True)
+
+print (f'{Cargar_Excel3_Sorted}')
+
+print (f'-' * 20)
+
+Cargar_Excel3_Sorted_Descending = Cargar_Excel3.sort_values(by='cinco', ascending=False)
+
+print (f'{Cargar_Excel3_Sorted_Descending}')
+
+import pandas as pd
+
+Ruta_Txt2 = 'C:\\Repo\\HolaMundo.txt'
+
+Cargar_Txt2 = pd.read_csv(Ruta_Txt2)
+
+print (f'{Cargar_Txt2.head()}')
+
+print (f'-' * 20)
+
+print (f'{Cargar_Txt2}')
+
+print (f'-' * 20)
+
+import pandas as pd
+
+Ruta_Csv2 = 'C:\\Repo\\Base_Datos.csv'
+
+Cargar_Csv2 = pd.read_csv(Ruta_Csv2)
+
+print (f'{Cargar_Csv2.head()}')
+
+print (f'-' * 20)
+
+import pandas as pd
+import requests
+import io
+
+Ruta_Html = 'https://en.wikipedia.org/wiki/Louisiana'
+
+headers = {'User-Agent' : 'Mozilla/5.0'}
+
+Response = requests.get(Ruta_Html, headers=headers)
+
+Leer_Html = io.StringIO(Response.text)
+
+Cargar_Html = pd.read_html(Leer_Html)
+
+print (f'{Cargar_Html[2].head()}')
+
+print (f'-' * 20)
+
+Lista_Array1 = [[1, 2, 3], [4, 5, 6]]
+
+print (f'{Lista_Array1[1][2]}')
+print (f'{Lista_Array1[1][:2]}')
+print (f'{Lista_Array1[1][2:]}')
+print (f'{Lista_Array1[0][::2]}')
+print (f'{Lista_Array1[1][::3]}')
+print (f'{Lista_Array1[1][2:3]}')
+print (f'{Lista_Array1[0][0:None]}')
+print (f'{Lista_Array1[0][:]}')
+
+print (f'-' * 20)
+
+Lista_Array2 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for i in range(len(Lista_Array2)):
+    for j in range(len(Lista_Array2[i])):
+        print (f'{Lista_Array2[i][j]}')
+
+print (f'-' * 20)
+
+for Fila in Lista_Array2:
+    for Elemento in Fila:
+        print (f'{Elemento}')
+
+print (f'-' * 20)
+
+import numpy as np
+
+Array1 = np.array([1, 2, 3])
+
+print (f'{Array1}')
+print (f'{Array1.ndim}') # 1
+print (f'{Array1.shape}') # 1x3
+print (f'{Array1.size}') # 3
+print (f'{Array1.dtype}') # int64
+print (f'{Array1[1]}')
+
+print (f'{Array1[:2]}')
+print (f'{Array1[2:]}')
+print (f'{Array1[::2]}')
+print (f'{Array1[::3]}')
+print (f'{Array1[1:2]}')
+print (f'{Array1[0:None]}')
+print (f'{Array1[:]}')
+print (f'{Array1[Array1 <= 2]}')
+
+print (f'-' * 20)
+
+Array2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+print (f'{Array2}')
+print (f'{Array2.ndim}') # 2
+print (f'{Array2.shape}') # 2x3
+print (f'{Array2.size}') # 6
+print (f'{Array2.dtype}') # int64
+print (f'{Array2[1, 0]}')
+
+print (f'{Array2[0, :2]}')
+print (f'{Array2[0, 2:]}')
+print (f'{Array2[1, ::2]}')
+print (f'{Array2[0, ::3]}')
+print (f'{Array2[1, 1:2]}')
+print (f'{Array2[:, 1]}')
+print (f'{Array2[0, 0:None]}')
+print (f'{Array2[0, :]}')
+print (f'{Array2[Array2 <= 2]}')
+
+Array2_Sorted = np.sort(Array2)
+Array2_Sorted_Mean = np.mean(Array2_Sorted)
+Array2_Sorted_Sum = np.sum(Array2_Sorted)
+
+Sumita1 = np.sum(Array2_Sorted, axis=0)
+Sumita2 = np.sum(Array2_Sorted, axis=1)
+Sumita3 = np.sum(Array2_Sorted[0, 0:None])
+Sumita4 = np.sum(Array2_Sorted[0, :])
+
+print (f'Acomodado: {Array2_Sorted}')
+print (f'Media: {round(Array2_Sorted_Mean, 2)}')
+print (f'Sumatoria: {Array2_Sorted_Sum}')
+
+print (f'El resultado de la sumita es {Sumita1}')
+print (f'El resultado de la sumita es {Sumita2}')
+print (f'El resultado de la sumita es {Sumita3}')
+print (f'El resultado de la sumita es {Sumita4}')
+
+print (f'-' * 20)
+
+Array3 = np.array([[['e', 's', 'u'], ['d', 'a', 'i']],              [['j', 'l', 'm'], ['n', 'o', 'r']]])
+
+print (f'{Array3}')
+print (f'{Array3.ndim}') # 3
+print (f'{Array3.shape}') # 2x2x3
+print (f'{Array3.size}') # 12
+print (f'{Array3.dtype}') # <U1
+print (f'{Array3[1, 0, 2]}')
+
+print (f'{Array3[1, 0, :2]}')
+print (f'{Array3[1, 0, 2:]}')
+print (f'{Array3[1, 1, ::2]}')
+print (f'{Array3[0, 1, ::3]}')
+print (f'{Array3[1, 0, 1:2]}')
+print (f'{Array3[0, :, 0]}')
+print (f'{Array3[1, 0, 0:None]}')
+print (f'{Array3[1, 0, :]}')
+print (f'{Array3[Array3 == "a"]}')
+
+print (f'-' * 20)
+
+Array4 = np.array([[[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [3, 2, 1]]],          [[[6, 5, 4], [9, 8, 7]], [[4, 6, 7], [3, 1, 9]]]])
+
+print (f'{Array4}')
+print (f'{Array4.ndim}') #  4
+print (f'{Array4.shape}') #2x2x2x3
+print (f'{Array4.size}') #24
+print (f'{Array4.dtype}') # int64
+print (f'{Array4[1, 0, 1, 2]}')
+
+print (f'{Array4[1, 0, 1, :2]}')
+print (f'{Array4[1, 0, 1, 2:]}')
+print (f'{Array4[1, 1, 0, ::2]}')
+print (f'{Array4[0, 0, 1, ::3]}')
+print (f'{Array4[0, 1, 0, 1:2]}')
+print (f'{Array4[1, 0, :, 2]}')
+print (f'{Array4[1, 0, 0, 0:None]}')
+print (f'{Array4[1, 0, 0, :]}')
+print (f'{Array4[Array4 <= 2]}')
+
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
+print (f'-' * 20)
