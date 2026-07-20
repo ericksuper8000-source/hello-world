4743de31da7f36589b54c3337691c6bb4e322de8
Erick_Dev <ericksuper80@gmail.com>
2026-07-19 18:34:54 -0600

    feature commit 1

--- Module_Own.py
+++ Module_Own.py
@@ -1,12 +1,21 @@
+'''Flotante1 = int(input(f'Ingrese un numero: '))'''
+
+'''Flotante2 = input(f'Ingrese una operacion tipo 4 * 3: ')'''
+
+'''Flotante3 = input(f'Ingrese su nombre: ')'''
+
+'''Flotante4 = input(f'Ingrese una cadena de texto: ')'''
+
 Diccionario_Poke = dict.fromkeys(['Poke1', 'Poke2', 'Poke3'])
 
 Set_Conjunto_Poke1 = {'Pikachu'}
 Set_Conjunto_Poke1.add('Graveler')
+
 Set_Conjunto_Poke2 = set({'Vaporeon'})
 
 Set_Conjunto_Poke1.update(Set_Conjunto_Poke2)
 
-for indice, elemento in enumerate(Set_Conjunto_Poke1, start=1):
+for elemento in Set_Conjunto_Poke1:
     if (elemento == 'Pikachu'):
         Diccionario_Poke['Poke1'] = elemento
     elif (elemento == 'Graveler'):
@@ -15,7 +24,7 @@ for indice, elemento in enumerate(Set_Conjunto_Poke1, start=1):
         Diccionario_Poke['Poke3'] = elemento
     else:
         continue
-
+    
 class Pokemon1():
     def __init__(self, Nombre, Tipo, Ataque):
         self.Nombre = Nombre
@@ -31,7 +40,7 @@ class Pokemon1():
         
 def Primera(Segunda): #type: ignore
     def Tercera():
-        print (f'ANTES')
+        print (f'ANTES DEL SALUDO')
         Segunda()
         
     return Tercera
@@ -42,15 +51,22 @@ def Saludar1():
     
 def Primera(Segunda): #type: ignore
     def Tercera(*args):
-        return Segunda('Juana La Cubana')
+        return Segunda('Carmelo')
         
     return Tercera
     
 @Primera
-def Saludar2(Nombre = 'Carmelo'):
+def Saludar2(Nombre = 'Juana La Cubana'):
     return Nombre
 
-def Saludar3(Nombre : str) -> str:
+def Primera(Segunda): #type: ignore
+    def Tercera(*args):
+        return Segunda('Carmel')
+        
+    return Tercera
+
+@Primera
+def Saludar3(Nombre:str) -> str:
     return Nombre
 
 def Primera(Segunda): #type: ignore
@@ -78,8 +94,8 @@ def Par(Numero):
     
 def Primera(Segunda): #type: ignore
     def Tercera(*args, **kwargs):
-        Nombre = 'Carmelo'
-        Sexo = 'MASCULINO'
+        Nombre = 'Juana La Cubana'
+        Sexo = 'FeMENIno'
         return Segunda(Nombre, Sexo)
         
     return Tercera
@@ -94,7 +110,7 @@ def Usuario1(Nombre, Sexo):
         
 def Primera(Segunda):
     def Tercera(*args):
-        return Segunda(74)
+        return Segunda(87)
         
     return Tercera
         
@@ -106,42 +122,38 @@ def Contrasena(Numero):
     c1 = Numero_Int - 2
     c2 = Numero_Int
     c3 = Numero_Int - 5
-    Password = f'{chars[c1]}{chars[c2]}{chars[c2]}{int(abs(Numero * c2))}'
+    Password = f'{chars[c1]}{chars[c2]}{chars[c3]}{int(abs(c2 * Numero))}'
     return Password
 
-Tupla_Poke = 'Ash', 'Brooke', 'Misty',
+Lista_Numeros = [1, 2]
+Lista_Numeros.append(3)
+Lista_Numeros.insert(4, 4)
+Lista_Numeros.extend([5])
 
-Lista_Numeros = [1, 2, 3, 4, 5]
+Tupla_Poke = tuple(('Ash', 'Brooke', 'Misty'))
 
 Variable_Funcion_Anonima1 = lambda Num1, Num2 : Num1 * Num2
-Variable_Funcion_Anonima2 = lambda Num :  Num * 2
+Variable_Funcion_Anonima2 = lambda Num : Num * 2
 Variable_Funcion_Anonima3 = filter(lambda Num : Num % 2 == 0, Lista_Numeros)
 
+Any_Par = any(num % 2 == 0 for num in Lista_Numeros)
+Lista_Par = [num for num in Lista_Numeros if num % 2 == 0]
+
 GLOBAL = 30
-
-'''Flotante1 = int(input(f'Ingrese un numero: '))'''
-
-'''Flotante2 = input(f'Ingrese una operacion tipo 4*3: ')'''
-
-'''Flotante3 = input(f'Ingrese su nombre: ')'''
-
-'''Flotante4 = input(f'Ingrese una cadena de texto: ')'''
 
 class Pokemon2():
     def __init__(self, Nombre, Tipo, Ataque):
         self.Nombre = Nombre
         self.Tipo = Tipo
         self.Ataque = Ataque
-        self.Cantidad = 18 * 2
-        self.Catched = not True
-
+        
     def Mostrar(self):
         print (f'Nombre: {self.Nombre}')
         print (f'Tipo: {self.Tipo}')
         print (f'Ataque: {self.Ataque}')
         
-Division_Flotante = 14 / 7
-        
+Division_Flotante = 14/ 7 
+  
 Lista1 = ['Erick', 'Josue', 'Perez', 'Gutierrez']
 Lista2 = [Division_Flotante, 200, 'Koala', not True]
 Lista3 = list([1, 2, 3, 4, 5])
